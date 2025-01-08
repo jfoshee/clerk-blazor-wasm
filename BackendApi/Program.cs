496631f0bd0c2e8af67bc5a016888515cfcd445e
Jacob Foshee <jacobf@gmail.com>
2025-01-08 10:53:57 -0600

    feat(backend): validate the `azp` claim

--- BackendApi/Program.cs
+++ BackendApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
@@ -6,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 var issuer = builder.Configuration["Jwt:Issuer"];
 if (string.IsNullOrWhiteSpace(issuer))
     throw new InvalidOperationException("MISSING CONFIG: 'Jwt:Issuer' is required");
+var authorizedParty = builder.Configuration["Jwt:AuthorizedParty"];
+if (string.IsNullOrWhiteSpace(authorizedParty))
+    throw new InvalidOperationException("MISSING CONFIG: 'Jwt:AuthorizedParty' is required");
 
 builder.Services.AddAuthorization(options =>
 {
@@ -15,11 +19,28 @@ builder.Services.AddAuthorization(options =>
 })
 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
+    // Validate the token issuer
     options.TokenValidationParameters.ValidIssuer = issuer;
-    // The JWT has azp but not aud, so we disable audience validation
-    options.TokenValidationParameters.ValidateAudience = false;
     options.Authority = issuer;
     options.ClaimsIssuer = issuer;
+    // The JWT has azp but not aud, so we disable audience validation
+    options.TokenValidationParameters.ValidateAudience = false;
+    // And instead validate the Authorized Party (AZP) claim
+    options.Events = new JwtBearerEvents
+    {
+        OnTokenValidated = context =>
+        {
+            // var aud = context.Principal?.FindFirstValue("aud");
+            // if (!string.IsNullOrEmpty(aud))
+            //     LogWarning("AUD Claim is present! Code must be updated to enable audience validation.");
+            var azp = context.Principal?.FindFirstValue("azp");
+            if (string.IsNullOrEmpty(azp))
+                context.Fail("AZP Claim is missing");
+            if (azp != authorizedParty)
+                context.Fail("AZP Claim does not match the configured authorized party");
+            return Task.CompletedTask;
+        }
+    };
     options.Validate();
 });
 
@@ -27,9 +48,12 @@ builder.Services.AddAuthorization(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Add CORS services
+// Add CORS policy
 builder.Services.AddCors(options =>
 {
+    // Note: This is a simple CORS policy that allows any origin, method, or header.
+    // If you are building a web API that needs to be consumed by a web application,
+    // you should be more restrictive.
     options.AddPolicy("AllowAllOrigins",
         builder =>
         {
